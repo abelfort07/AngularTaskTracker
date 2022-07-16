e8f000b301415f471065efffcd8298d45f58b01c
Angelo Belfort <belfortangelo93@gmail.com>
2022-07-16 16:36:40 -0400

    Create button component with event and Tasks component

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from "./Task"
+
+export const TASK: Task[] = [
+      {
+        "id": 1,
+        "text": "Appointment with the doctor",
+        "day": "23 March at 2h 30 PM",
+        "reminder": true
+      },
+      {
+        "id": 2,
+        "text": "Brainstorming with other Developers",
+        "day": "30 March at 4h 30 PM",
+        "reminder": true
+      },
+      {
+        "text": "Reading a PD Book",
+        "day": "Today at 9h PM",
+        "reminder": false,
+        "id": 3
+      }
+]
