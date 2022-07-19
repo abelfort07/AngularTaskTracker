6b904f1c59cf31b8bfa853f6755ecf9cd648e9bf
Angelo Belfort <belfortangelo93@gmail.com>
2022-07-19 12:16:14 -0400

    create addTask, About, Footer, UIService and finish the application

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -2,17 +2,41 @@ import { Injectable } from '@angular/core';
 import { Observable} from 'rxjs';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { Task } from 'src/app/Task';
-import { TASK } from '../mock-tasks';
+
+const httpOptions = {
+  headers : new HttpHeaders({
+    'Content-Type' : 'application/json'
+  })
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  //API Url to connect to the tasks from the Json Server
   private apiUrl = 'http://localhost:5000/tasks'
 
   constructor(private http:HttpClient) { }
 
+  // Get All the Tasks from Json server
   getTask(): Observable<Task[]>{
     return this.http.get<Task[]>(this.apiUrl);
+  }
+
+  // Delete a specific task from server using ID
+  deleteTask(task: Task) : Observable<Task>{
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.delete<Task>(url);
+  }
+
+  //Update Task's Reminder
+  updateTaskReminder(task:Task):Observable<Task>{
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.put<Task>(url, task, httpOptions);
+  }
+
+  //Add Task
+  addTask(task:Task):Observable<Task>{
+    return this.http.post<Task>(this.apiUrl, task, httpOptions);
   }
 }
